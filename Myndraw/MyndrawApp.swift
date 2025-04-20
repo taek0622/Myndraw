78902464780bcf58b893f77eb5f67865e9c93a64
taek0622 <min4644@gmail.com>
2025-04-20 15:20:28 +0900

    Init: Initial Project
    - 프로젝트 추가

---
+++ Myndraw/MyndrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyndrawApp.swift
+//  Myndraw
+//
+//  Created by 김민택 on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MyndrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
